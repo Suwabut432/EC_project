0c731bc9c985712d829c314e1e5f33f9fc6bbcf0
Suwabut <suwabut.sb@gmail.com>
2025-09-12 18:17:28 +0500

    maseg

--- config/mongoose-conection.js
+++ config/mongoose-conection.js
@@ -3,9 +3,7 @@ const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 require("dotenv").config();
 
-mongoose
-.connect(`${process.env.connection}/scatch`)
-.then(function(){
-    dbgr("✅ MongoDB Connected");
-})  .catch(err => console.error("❌ MongoDB connection error:", err))
-
+mongoose.connect(process.env.MONGO_URL)
+    .then(function () {
+        dbgr("✅ MongoDB Connected");
+    }).catch(err => console.error("❌ MongoDB connection error:", err))
