a92d40bd932e64cab73d37f330895e4ee8e7de62
Suwabut <suwabut.sb@gmail.com>
2025-09-13 14:07:02 +0500

    no login

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const upload = require("../config/multer-config");
 const productModel = require("../models/product-model");
 
-router.get("/", IsLoggedIn, function (req, res) {
+router.get("/", function (req, res) {
     res.render("admin");
 })
 
