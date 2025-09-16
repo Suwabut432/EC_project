9754a1dc20a06a06a72538523e9897db41b29d7c
Suwabut <suwabut.sb@gmail.com>
2025-09-16 14:39:24 +0500

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
@@ -28,7 +28,7 @@ app.use(session({
 app.use(flash());
 
 const ownerRouter = require("./routes/ownerRouter");
-const userRouter  = require("./routes/userRouter");
+const userRouter = require("./routes/userRouter");
 const productsRouter = require("./routes/productRouter");
 const index = require("./routes/index");
 
@@ -41,4 +41,4 @@ app.use("/", index);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>console.log("🚀 Server running on", PORT));
+app.listen(PORT, () => console.log("🚀 Server running on", PORT));
